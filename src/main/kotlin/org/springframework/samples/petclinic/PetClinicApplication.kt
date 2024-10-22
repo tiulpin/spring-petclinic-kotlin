72917efb21c0105150cdec70df14334fe02a738c
Viktor <viktor@tiulp.in>
2024-10-22 18:09:33 +0200

    Refactor the project to use Ktor instead of Spring
    Related to #1
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/tiulpin/spring-petclinic-kotlin/issues/1?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/main/kotlin/org/springframework/samples/petclinic/PetClinicApplication.kt
+++ src/main/kotlin/org/springframework/samples/petclinic/PetClinicApplication.kt
@@ -1,32 +1,32 @@
-/*
- * Copyright 2002-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- * @author Antoine Rey
- */
-@SpringBootApplication(proxyBeanMethods = false)
-class PetClinicApplication
-
-fun main(args: Array<String>) {
-    runApplication<PetClinicApplication>(*args)
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
+        install(StatusPages) {
+            exception<Throwable> { cause ->
+                call.respond(HttpStatusCode.InternalServerError, cause.localizedMessage)
+            }
+        }
+        routing {
+            get("/") {
+                call.respond(HttpStatusCode.OK, "Welcome to Ktor PetClinic!")
+            }
+        }
+    }.start(wait = true)
 }
--- src/main/kotlin/org/springframework/samples/petclinic/system/CrashController.kt
+++ src/main/kotlin/org/springframework/samples/petclinic/system/CrashController.kt
@@ -1,38 +1,33 @@
-/*
- * Copyright 2002-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.system
 
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.StatusPages
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-/**
- * Controller used to showcase what happens when an exception is thrown
- *
- * @author Michael Isvy
- * @author Antoine Rey
- * <p/>
- * Also see how a view that resolves to "5xx" has been added ("5xx.html").
- */
-@Controller
-class CrashController {
-
-    @GetMapping("/oups")
-    fun triggerException() {
-        throw RuntimeException(
-                "Expected: controller used to showcase what happens when an exception is thrown")
+fun Application.module() {
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
     }
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.respond(HttpStatusCode.InternalServerError, cause.localizedMessage)
+        }
+    }
+    routing {
+        get("/oups") {
+            throw RuntimeException("Expected: controller used to showcase what happens when an exception is thrown")
+        }
+    }
+}
 
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
 }
